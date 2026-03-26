6fefefc4bb9bc172afe9a9b05198919180181e5b
AhmedSalem <medsalem.sb03@gmail.com>
2026-03-26 11:53:36 +0000

    Spring Boot project

---
+++ voitures-backend/src/main/java/com/workshop/voitures/VoituresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.voitures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoituresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoituresBackendApplication.class, args);
+	}
+
+}
---
+++ voitures-backend/src/test/java/com/workshop/voitures/VoituresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.voitures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoituresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
